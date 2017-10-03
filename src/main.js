ac17db86dac04c746bee21733a5109a781ca75e5
ErryZHL <31409840+ErryZHL@users.noreply.github.com>
2017-10-03 16:32:09 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ const LineAPI = require('./api');
 const { Message, OpType, Location } = require('../curve-thrift/line_types');
 let exec = require('child_process').exec;
 
-const myBot = ['u78b179f959eba71ec2de09233281c49e','uc93c736a8b385208c2aa7aed58de2ceb','u236b88bf1eac2b90e848a6198152e647','u763977dab29cbd6fa0cbfa9f159b768b'];
-
+const myBot = ['u599f6340a811d2f6543762d638e8ad36'];
 
 function isAdminOrBot(param) {
     return myBot.includes(param);
